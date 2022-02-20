088fc506a7380f6376b642db886af0edaed3703d
Dawid Starzyk <89983699+JustD1d1t@users.noreply.github.com>
2022-02-20 21:06:28 +0100

    feat: express server (#1)
    * feat: express server
    
    * styles: remove unnecessary comments
    
    * refactor: listen error handling, merge app with index
    
    * refactor: error handler, git ignore, headers
    
    * refactor: error handling
    
    * refactor: headers, english lang

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app
+  .listen(port, () => {
+    console.log(`App listening on port ${port}`);
+  })
+  .on("error", (err) => {
+    console.error("An error occurred while starting the server " + err);
+  });
