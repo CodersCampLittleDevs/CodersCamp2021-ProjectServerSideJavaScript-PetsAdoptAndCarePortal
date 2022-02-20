f2dac4e3dfe916a6d648c184197f97a53f01d2bd
JustD1d1t <starzyk.dawid.0826@gmail.com>
2022-02-20 20:59:22 +0100

    refactor: headers, english lang

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", ["*"]);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
@@ -11,8 +11,8 @@ app.use(function (req, res, next) {
 
 app
   .listen(port, () => {
-    console.log(`Serwer uruchomiony na porcie ${port}`);
+    console.log(`App listening on port ${port}`);
   })
   .on("error", (err) => {
-    console.error("Wystąpił błąd przy uruchomieniu serwera " + err);
+    console.error("An error occurred while starting the server " + err);
   });
