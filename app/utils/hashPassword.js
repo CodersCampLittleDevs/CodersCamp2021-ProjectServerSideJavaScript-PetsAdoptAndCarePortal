040afd6706816f6d361060c1b55a5e7abaf05d92
Maciej Ponicki <72940506+MaciekPon@users.noreply.github.com>
2022-03-07 20:03:05 +0100

    feature/NJS-3 (#6)
    * feat: add authorization user schema
    add function to validate email
    
    * fix: add changes from code review
    change files names
    
    * fix: change user schema
    change import name in hashPassword file

---
+++ app/utils/hashPassword.js
@@ -0,0 +1,8 @@
+import bcrypt from "bcrypt";
+
+export const hashPassword = (req, res, next) => {
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  req.body.password = hash;
+  next();
+};
