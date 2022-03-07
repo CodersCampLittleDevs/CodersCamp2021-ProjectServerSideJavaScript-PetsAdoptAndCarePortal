145dff1b9cf26e1102690d4b62a51ef791718b9d
DawidZlobecki <96154754+DawidZlobecki@users.noreply.github.com>
2022-03-07 20:03:21 +0100

    Njs 4 ".env file" (#3)
    * add .env file to gitignore
    
    * add readme
    
    * add file .env.example
    
    * Added to .env variables
    
    * Added import dotenv to index
    
    * Changed port
    
    * fixed dotenv import
    
    * Update readme
    
    * fix
    
    * removed redundant comment
    
    * ordered imports

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
+import dotenv from "dotenv";
 import express from "express";
 import { authRouter } from "./routes/authRoutes.js";
 import { announcementRouter } from "./routes/announcements-routes.js";
 
+dotenv.config();
+
 const port = process.env.PORT || 3000;
 const app = express();
-
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -14,7 +16,7 @@ app.use(function (req, res, next) {
 });
 
 app.use("/auth", authRouter);
-app.use("/announcements", announcementRouter)
+app.use("/announcements", announcementRouter);
 
 app
   .listen(port, () => {
