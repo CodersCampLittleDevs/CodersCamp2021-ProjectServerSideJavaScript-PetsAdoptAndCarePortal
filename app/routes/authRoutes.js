a1f606de785fdd4af1a0493f1974ad174699cb66
dawidzlobecki <dawidzlobecki2@gmail.com>
2022-03-09 11:58:34 +0100

    feat: add login validations

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -1,10 +1,21 @@
 import express from "express";
 export const authRouter = express.Router();
+import User from "../models/UserSchema";
 
 authRouter.post("/login", (req, res, next) => {
-  res.status(200).json({
-    message: "Handling POST requests to /auth/login",
-  });
+  // const {error} = loginValidation(req.body);
+  // if (error) return res.status(400).send(error.details[0].message);
+
+  //Checking if email exists in database
+
+  const emailExists = await User.findOne({ email: req.body.user });
+  const userExists = await User.findOne({ username: req.body.user });
+  if (!emailExists || !userExists)
+    return res.status(400).send("Invalid login name!");
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid password");
+
+  res.send("Logged In!");
 });
 
 authRouter.post("/forgot", (req, res) => {
