ab6f0289f188b43850795e36f37a8d359fdacfbc
JustD1d1t <starzyk.dawid.0826@gmail.com>
2022-03-11 16:21:02 +0100

    feat: get user data

--- app/routes/userRouter.js
+++ app/routes/userRouter.js
@@ -1,13 +1,10 @@
 import express from "express";
 import { auth } from "../middleware/verifyToken.js";
+import { getUserData } from "../controllers/userController.js";
 
 export const userRouter = express.Router();
 
-userRouter.get("/:aid", auth, (req, res) => {
-  res.status(200).json({
-    message: "get user data",
-  });
-});
+userRouter.get("/:uid", getUserData);
 
 userRouter.put("/:aid", auth, (req, res) => {
   res.status(200).json({
